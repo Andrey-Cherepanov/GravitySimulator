bf5e6723f4d79d89f114718657ebf42f9e601c48
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-11-02 20:09:45 +0300

    draw a spacecraft on click

--- main.py
+++ main.py
@@ -34,16 +34,34 @@ class Spacecraft:
 def main():
     running = True
     clock = pygame.time.Clock()
+    objects = []
+    temp_obj_pos = None
 
     while running:
         clock.tick(FPS)
 
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if not temp_obj_pos:
+                    temp_obj_pos = mouse_pos
+                else:
+                    t_x, t_y = temp_obj_pos
+                    obj = Spacecraft(t_x, t_y, 0, 0, SHIP_MASS)
+                    objects.append(obj)
+                    temp_obj_pos = None
+
 
         window.blit(bg, (0,0))
         window.blit(planet, (WIDTH//2 - PLANET_SIZE, HEIGHT//2 - PLANET_SIZE))
+        if temp_obj_pos:
+            pygame.draw.line(window, COLORS['WHITE'], temp_obj_pos, mouse_pos, 2)
+            pygame.draw.circle(window, COLORS['RED'], temp_obj_pos, SHIP_SIZE)
+
+        for obj in objects:
+            obj.draw()
         pygame.display.update()
 
     pygame.quit()
